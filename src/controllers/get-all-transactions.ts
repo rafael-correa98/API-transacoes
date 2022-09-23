81997d9b3fe9d4abd43a65f64abb5dc4bf4304d6
rafael-correa98 <rafaelcorrean98@gmail.com>
2022-09-23 10:08:44 -0300

    capturar todas transacoes

--- src/controllers/get-all-transactions.ts
+++ src/controllers/get-all-transactions.ts
@@ -6,9 +6,30 @@ export class GetAllTransactionsController {
     const {userId} = request.params
     
     const userIndex = usersDB.findIndex(user => user.id === userId)
+
     
     let transactions = usersDB[userIndex].transactions.map(transaction => transaction.toJson())
     
-    return response.json(transactions)
+    const valueIncome = transactions.filter(transaction => transaction.type === "income")
+    .map(transaction =>{
+      return transaction.value
+    })
+    .reduce((prev, curr) => prev + curr, 0)
+
+    const valueOutcome = transactions.filter(transaction => transaction.type === "outcome")
+    .map(transaction =>{
+      return transaction.value
+    })
+    .reduce((prev, curr) => prev + curr, 0)
+
+    const balance = {
+      income: valueIncome,
+      outcome: valueOutcome,
+      total: valueIncome - valueOutcome
+    }
+
+    const data = [transactions, balance]
+    
+    return response.json(data)
   }
 }
